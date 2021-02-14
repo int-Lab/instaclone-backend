96662d8a3226fad37ae9ad20e620d174edd35a0c
int <64084503+chul0721@users.noreply.github.com>
2021-02-14 19:20:06 +0900

    just a review of graphql

--- server.ts
+++ server.ts
@@ -1,22 +1,44 @@
 import { ApolloServer, gql } from 'apollo-server';
 
 const typeDefs:any = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
+
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+
+  type Mutation {
+    createMovie(title:String!): Boolean
+    deleteMovie(title:String!): Boolean
   }
 `;
 
 const resolvers:any = {
   Query: {
-    hello: () => 'world',
+    movies: () => [],
+    movie: () => [{ title: "Hello", year: 2021 }]
   },
+  Mutation: {
+    createMovie: (root:any, {title}:any):any => {
+      console.log(title);
+      return true
+    },
+    deleteMovie: (root:any, {title}:any):any => {
+      console.log(title);
+      return true;
+    }
+  }
 };
 
-const server:any = new ApolloServer({
+const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-server.listen().then(({ url }:any): any => {
+server.listen().then(({ url }) => {
   console.log(`Server is running on port ${url}`);
 });
